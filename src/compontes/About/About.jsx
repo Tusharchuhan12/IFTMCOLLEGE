9da9771195cdb99eb75321d463fef8c421529e8e
tushar chauhan <tusharchauhan120104@zohomail.in>
2026-01-19 01:26:07 +0530

    changes

--- src/compontes/About/About.jsx
+++ src/compontes/About/About.jsx
@@ -15,7 +15,7 @@ function About({ setplay }) {
     <div className='about-right'>
         <h2>ABOUT UNIVERSITY</h2>
           <h1>IFTM University</h1>
-          <p>• Offers 82 number of Diploma, Undergraduate, Postgraduate and Doctoral Programmes in Engineering, Business Management, Pharmacy, Biotechnology, Microbiology, Arts, Sciences, Commerce, Hotel Management & Catering Technology, Law, Education, Journalism & Mass Communication, Agricultural Science & Engineering and Computer Science & Applications, etc..</p>
+          <p>• Offers 827 number of Diploma, Undergraduate, Postgraduate and Doctoral Programmes in Engineering, Business Management, Pharmacy, Biotechnology, Microbiology, Arts, Sciences, Commerce, Hotel Management & Catering Technology, Law, Education, Journalism & Mass Communication, Agricultural Science & Engineering and Computer Science & Applications, etc..</p>
           <p>• Besides academics, sports and cultural activities, the University contribute towards the holistic development of students..</p>
        
         <p>With a focus on innovation, hands-on learning, and personalized mentorship, our programs prepare aspiring educators to make a meaningful impact in classrooms, schools, and communities.</p>
